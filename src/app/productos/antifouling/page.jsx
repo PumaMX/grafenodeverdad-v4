2b2a58decf1c47d70671f1e2b90cee721e9681bc
Josè Luis Rodriguez <joseluisrodriguez@192.168.1.67>
2025-10-26 22:00:23 -0600

    Actualizacion Gr-v4

--- src/app/productos/antifouling/page.jsx
+++ src/app/productos/antifouling/page.jsx
@@ -1,6 +1,6 @@
 'use client'
 
-export default function Anti-biofouling() {
+export default function Antibiofouling() {
   return (
     <main className="p-8 max-w-6xl mx-auto">
       <h1 className="text-3xl font-bold mb-6 text-center">Recubrimientos Antifouling</h1>
